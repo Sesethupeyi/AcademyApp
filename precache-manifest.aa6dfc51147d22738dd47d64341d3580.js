77e42a8f2f255e0f9568300e0ad274e26e4f3f61
SesethuPeyi <sesethupeyi@gmail.com>
2019-11-23 17:28:25 +0200

    Updates

---
+++ precache-manifest.aa6dfc51147d22738dd47d64341d3580.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faae6d77334ed660d07b5c5e6ce76d2c",
+    "url": "/AcademyApp/index.html"
+  },
+  {
+    "revision": "43671248fc3cd8aa6e0a",
+    "url": "/AcademyApp/static/css/main.cef1233f.chunk.css"
+  },
+  {
+    "revision": "b8336bced874f0680254",
+    "url": "/AcademyApp/static/js/2.58c209ce.chunk.js"
+  },
+  {
+    "revision": "43671248fc3cd8aa6e0a",
+    "url": "/AcademyApp/static/js/main.3952e6d4.chunk.js"
+  },
+  {
+    "revision": "0477339eeadd30bed663",
+    "url": "/AcademyApp/static/js/runtime-main.fc4d4515.js"
+  },
+  {
+    "revision": "0393ecacad02c0eb8a8ebd8a0ca3e9af",
+    "url": "/AcademyApp/static/media/Algebra.0393ecac.jpg"
+  },
+  {
+    "revision": "f11c5c75580f5db7c620c9a463b53604",
+    "url": "/AcademyApp/static/media/Analytical.f11c5c75.jpg"
+  },
+  {
+    "revision": "00c73aeb1eba3955c6b96e6744440dab",
+    "url": "/AcademyApp/static/media/Calculus.00c73aeb.jpg"
+  },
+  {
+    "revision": "79766df4c0c2cc108190f092dda82e93",
+    "url": "/AcademyApp/static/media/Cubic.79766df4.jpg"
+  },
+  {
+    "revision": "c1019efaf2e7bd686e6d26f243fb2d8b",
+    "url": "/AcademyApp/static/media/Sequence and Series.c1019efa.jpg"
+  },
+  {
+    "revision": "b57377d415c338d4d709214c76fb0b02",
+    "url": "/AcademyApp/static/media/Trigonometry.b57377d4.jpg"
+  },
+  {
+    "revision": "5236e097f6f9576fc4b5f549c3807e20",
+    "url": "/AcademyApp/static/media/anal1.5236e097.png"
+  },
+  {
+    "revision": "84159a67c1d0d9d470c181e8d92eab98",
+    "url": "/AcademyApp/static/media/calc1.84159a67.png"
+  },
+  {
+    "revision": "5f41c9c7857a881fb0e80dfbd4609c54",
+    "url": "/AcademyApp/static/media/math.5f41c9c7.jpg"
+  },
+  {
+    "revision": "9641f7da65643465d317f49059cf9d55",
+    "url": "/AcademyApp/static/media/pol1.9641f7da.jpg"
+  }
+]);
